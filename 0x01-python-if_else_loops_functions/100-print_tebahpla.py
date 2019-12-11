78aa1a7a281e1d9beba21f9b4d3672878a7a1889
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-11 14:52:04 +0000

    Fixes

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((i, i - 32)[i % 2]), end="")
+    i = (i, i - 32)[i % 2]
+    print("{:c}".format(i), end="")
