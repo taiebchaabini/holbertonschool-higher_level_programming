5531e9406f44d1a34108cc6f643a22ae9ff7a30a
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-12 19:35:44 +0000

    Fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     nbarg = len(sys.argv) - 1
-    print("{:d} argument".format(nbarg)+(":", ".")[nbarg == 0])
+    print("{:d} argument".format(nbarg)+((":","s:")[nbarg > 1], "s.")[nbarg == 0])
     for i in range(1, nbarg + 1):
         print("{:d}: {:s}".format(i, sys.argv[i]))
