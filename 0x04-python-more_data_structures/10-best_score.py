20d53616244337dbaec4db5b44c7a4e24ef52949
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-21 20:56:02 +0000

    Fix to key variable default value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if (a_dictionary is None):
         return None
-    key = -99
+    key = list(a_dictionary.values())[0]
     res = ""
     for i, v in a_dictionary.items():
         if (v >= key):
