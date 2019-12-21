5e1acf1b60e3c5d037071ba5fdc3d095d4683d7b
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-21 15:01:02 +0000

    Changing code, pep8 issue

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    cond = lambda x:replace if x == search else x
     for i in range(len(my_list)):
-        new_list.append(cond(my_list[i]))
+        new_list.append(replace if my_list[i] == search else my_list[i])
     return (new_list)
