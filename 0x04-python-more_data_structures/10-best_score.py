c7bdf7f82f71bcc7751eb95c0d9f067d4b93cbc4
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-21 19:33:59 +0000

    Changing if statement condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if (a_dictionary is None):
         return None
-    key = -9999999999999
+    key = -9999999
     res = ""
     for i, v in a_dictionary.items():
-        if (v > key):
+        if (v >= key):
             key = v
             res = i
     return res
