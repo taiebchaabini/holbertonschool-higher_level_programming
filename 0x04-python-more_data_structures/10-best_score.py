7d55754f9f462a0a475c4058ce0fa8110dc69e2a
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-21 19:31:28 +0000

    First commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if (a_dictionary is None):
+        return None
+    key = -9999999999999
+    res = ""
+    for i, v in a_dictionary.items():
+        if (v > key):
+            key = v
+            res = i
+    return res
