e3fdba0233374c24906dcfc54a7242f8117e5c58
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-21 21:36:06 +0000

    Changing loops, key default value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,11 @@
 def best_score(a_dictionary):
     if (a_dictionary is None):
         return None
-    key = list(a_dictionary.values())[0]
+    key = -999999999999999999999
     res = ""
-    for i, v in a_dictionary.items():
-        if (v >= key):
+    for i in a_dictionary:
+        v = a_dictionary[i]
+        if (v > key):
             key = v
             res = i
     return res
