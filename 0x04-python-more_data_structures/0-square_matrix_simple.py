2935f0360c98a6ee80309ff905e4c34d1cb09cbc
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-21 14:30:41 +0000

    Changing the whole code to (list, map, lambda)

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None or len(matrix) == 0:
-        return
     new_matrix = []
     for i in range(len(matrix)):
-        new_matrix.append([row[i] for row in matrix])
-    for i in range(len(matrix)):
-        for b in range(len(matrix[i])):
-            new_matrix[i][b] = matrix[i][b] * matrix[i][b]
-    return new_matrix
+        new_matrix.append(list(map(lambda x: x*x, matrix[i])))
+    return (new_matrix)
