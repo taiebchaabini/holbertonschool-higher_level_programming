fa720cf22e29e0b9783fe982798015e1d598995c
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-21 21:13:47 +0000

    Fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,14 @@ def best_score(a_dictionary):
     if (a_dictionary is None):
         return None
     key = list(a_dictionary.values())[0]
+    #key = -99999999999999999999
     res = ""
+    c = 0
     for i, v in a_dictionary.items():
         if (v >= key):
             key = v
             res = i
+            c += 1
+    if (c == len(list(a_dictionary.values()))):
+        return None
     return res
