c6fca20ceea283f632e159fafe8e2b5af22bcbd9
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-22 01:05:01 +0000

    Changing lambda

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: (x[0]*x[0], x[1]*x[1], x[2]*x[2]), matrix)))
+    return (list(map(lambda a: list(map(lambda x: x*x, a)), matrix)))
