e6f34b194a0499f8f12cb5f5640337af661c3804
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-22 00:24:40 +0000

    Pep8 fixing missing space

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-   new_matrix = list(map(lambda x:(x[0]*x[0],x[1]*x[1],x[2]*x[2]),matrix))
-   return (new_matrix)
+    new_matrix = list(map(lambda x: (x[0]*x[0], x[1]*x[1], x[2]*x[2]), matrix))
+    return (new_matrix)
