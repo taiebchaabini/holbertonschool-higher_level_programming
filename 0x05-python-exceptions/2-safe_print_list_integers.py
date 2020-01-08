46cced44d4dbb3378532d2a68422dca38ef66155
taiebchaabini <taiebchaabini@gmail.com>
2020-01-08 19:31:44 +0100

    Removing unneeded iterator

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    for c in range (x):
+    for c in range(x):
         try:
             print("{:d}".format(my_list[c]), end="")
-            c += 1
         except (TypeError, ValueError):
             c -= 1
             pass
