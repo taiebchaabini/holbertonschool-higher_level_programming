8d022e61fd4f475dc6362c1e28c856b8af69d85b
taiebchaabini <taiebchaabini@gmail.com>
2020-01-08 22:16:16 +0100

    Changing except

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as ve:
+    except Exception as ve:
         print("Exception:", ve, file=sys.stderr)
         return False
     return True
