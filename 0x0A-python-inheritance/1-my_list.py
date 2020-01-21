c885fa9d53e3717d9959657c077a96d20f6caa00
taiebchaabini <taiebchaabini@gmail.com>
2020-01-21 21:57:26 +0000

    Adding comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+    class MyList that inherits from list
+"""
 
 
 class MyList(list):
+    """
+        prints the list, but sorted (ascending sort)
+        all the elements of the list will be of type <int>
+    """
     def print_sorted(self):
         print(sorted(self))
 
