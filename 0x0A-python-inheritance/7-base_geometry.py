2a1f8c2a4e18be34d72985849877129b3e598f91
taiebchaabini <taiebchaabini@gmail.com>
2020-01-21 22:16:29 +0000

    Add comments

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+"""
+   class BaseGeometry (based on 6-base_geometry.py).
+"""
 
 
 class BaseGeometry:
+    """
+        class BaseGeometry (based on 6-base_geometry.py).
+    """
     def area(self):
+        """
+           raises an Exception : area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+           name is always a string
+           if value is not an integer: raise a TypeError exception
+           if value is less or equal to 0: raise a ValueError exception
+        """
         if type(name) is not str:
             raise TypeError("{} must be a string".format(name))
         if type(value) is not int:
