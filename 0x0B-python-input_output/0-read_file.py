e630a2595fd66343c753c007cbd212a558703ae9
taiebchaabini <taiebchaabini@gmail.com>
2020-01-22 18:17:41 +0000

    Adding encoding utf8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     """
        function that reads a text file (UTF8) and prints it to stdout:
     """
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding='utf-8') as f:
         print(f.read(), end='')
