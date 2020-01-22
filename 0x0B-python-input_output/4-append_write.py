54447c3ac7afccd618113bc6c3b97f8d25f831d7
taiebchaabini <taiebchaabini@gmail.com>
2020-01-22 18:21:09 +0000

    Adding encoding utf8

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -11,5 +11,5 @@ def append_write(filename="", text=""):
        and returns
        the number of characters added.
     """
-    with open(filename, "a") as f:
+    with open(filename, "a", encoding='utf-8') as f:
         return f.write(text)
