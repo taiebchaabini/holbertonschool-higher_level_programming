9366256b828c30791ade82e6b9f0805f03b44b57
taiebchaabini <taiebchaabini@gmail.com>
2020-01-22 21:54:56 +0000

    Removing import

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -2,11 +2,10 @@
 """
     function that returns the dictionary description with simple data
 """
-import json
 
 
 def class_to_json(obj):
     """
         function that returns the dictionary description with simple data
     """
-    return dict(obj.__dict__)
+    return obj.__dict__
