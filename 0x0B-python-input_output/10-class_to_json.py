2f0882c128bb0fe31783cbec9f05a5fed10805c2
taiebchaabini <taiebchaabini@gmail.com>
2020-01-22 21:52:34 +0000

    Removing comments

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
     function that returns the dictionary description with simple data
-    structure (list, dictionary, string, integer and boolean) for
-    JSON serialization of an object
 """
 import json
 
@@ -10,7 +8,5 @@ import json
 def class_to_json(obj):
     """
         function that returns the dictionary description with simple data
-        structure (list, dictionary, string, integer and boolean) for
-        JSON serialization of an object
     """
-    return obj.__dict__
+    return dict(obj.__dict__)
