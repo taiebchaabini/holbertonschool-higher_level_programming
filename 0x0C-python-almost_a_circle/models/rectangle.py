71a5b86bf713bd05e6f727976189235ac1a4380b
taiebchaabini <taiebchaabini@gmail.com>
2020-01-26 17:21:29 +0000

    First commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+   Rectangle that inherits from Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+       Rectangle that inherits from Base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        if id is not None:
+            self.id = id
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
