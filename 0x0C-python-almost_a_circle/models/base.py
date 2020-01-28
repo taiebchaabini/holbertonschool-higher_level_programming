0433c5d4a23390bb234a699393854a5b0dc5c2e9
taiebchaabini <taiebchaabini@gmail.com>
2020-01-28 19:24:31 +0000

    Changing condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,6 @@ class Base:
         """
            returns the JSON string representation of list_dictionaries
         """
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
