fc097fc04f2e9c33d111294d794bca33921a66a4
taiebchaabini <taiebchaabini@gmail.com>
2020-01-28 14:40:34 +0000

    New changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,6 +11,8 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__()
+        if id is not None and type(id) is int:
+            self.id = id
         self.width = width
         self.height = height
         self.x = x
