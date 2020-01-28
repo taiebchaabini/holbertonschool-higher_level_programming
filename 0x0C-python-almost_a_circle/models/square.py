e00aa254f7b7e2999488aa9d3d7c93ad4bcecd6d
taiebchaabini <taiebchaabini@gmail.com>
2020-01-28 18:34:32 +0000

    Adding update method to square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,3 +35,20 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+            args is the list of arguments - no-keyworded arguments
+              1st argument should be the id attribute
+              2nd argument should be the size attribute
+              3rd argument should be the x attribute
+              4th argument should be the y attribute
+           *kwargs is skipped if *args exists and is not empty
+        """
+        lena = len(args)
+        largs = ('id', 'size', 'x', 'y')
+        for i in range(lena):
+            setattr(self, largs[i], args[i])
+        if lena == 0:
+            for i in kwargs:
+                setattr(self, i, kwargs[i])
