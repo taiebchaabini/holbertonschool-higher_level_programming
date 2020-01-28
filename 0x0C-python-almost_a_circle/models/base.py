ab797fad02467592a6a673caa8507cba6aeb7155
taiebchaabini <taiebchaabini@gmail.com>
2020-01-28 19:27:05 +0000

    Adding static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
            returns the JSON string representation of list_dictionaries
