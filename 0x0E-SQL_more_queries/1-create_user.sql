73350c129fa6543ea2a1200b979c0e881aa94a2a
taiebchaabini <taiebchaabini@gmail.com>
2020-03-04 21:38:41 +0000

    First commit - new file

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- script that creates the MYSQL server user_0d_1
+-- Login creation for user
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- GRANT all privileges to USER
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
