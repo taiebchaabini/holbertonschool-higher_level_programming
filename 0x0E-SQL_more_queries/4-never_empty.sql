a3d64296dbc8e7fecd85c93d15ea49866f56c2c9
taiebchaabini <taiebchaabini@gmail.com>
2020-03-05 10:44:58 +0000

    First commit - new file

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- script that creates the table id_not_null on your MySQL server.
+-- CREATE TABLE id_not_null IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS id_not_null(
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+);
