f601207bf6f53f23a8b94dcaa740b59a4641c629
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 11:59:50 +0000

    Changing KEYWORDS TO uppercase

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT tg.name as genre, COUNT(tg.name) as number_of_shows FROM tv_shows t LEFT JOIN tv_show_genres tsg ON t.id = tsg.show_id LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id WHERE tg.name IS NOT NULL GROUP BY tsg.genre_id ORDER BY number_of_shows DESC;
+SELECT tg.name AS genre, COUNT(tg.name) AS number_of_shows FROM tv_shows t LEFT JOIN tv_show_genres tsg ON t.id = tsg.show_id LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id WHERE tg.name IS NOT NULL GROUP BY tsg.genre_id ORDER BY number_of_shows DESC;
