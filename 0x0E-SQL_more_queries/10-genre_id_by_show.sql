7aa16cdfba97c9a0101d76572e6f3d4093fdd513
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 09:26:55 +0000

    Changing keywords to uppercase

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
-select t.title, tsg.genre_id FROM tv_shows t LEFT JOIN tv_show_genres tsg ON t.id = tsg.show_id WHERE tsg.genre_id IS NOT NULL ORDER BY t.title, tsg.genre_id ASC;
+SELECT t.title, tsg.genre_id FROM tv_shows t LEFT JOIN tv_show_genres tsg ON t.id = tsg.show_id WHERE tsg.genre_id IS NOT NULL ORDER BY t.title, tsg.genre_id ASC;
