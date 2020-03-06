ca4bdabb86221878edcd1a718986156e856247ba
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 09:12:30 +0000

    First commit - new file

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- SELECT cities.id - cities.name - states.name 
+SELECT c.id, c.name, s.name FROM cities c LEFT JOIN states s ON c.state_id = s.id
