972afb3c427b5a0cd39fc68895332576708d58e7
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 08:25:08 +0000

    Removing comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- script that lists all privileges of the MYSQL users
 -- script that lists all privileges of user "user_0d_1"
 SHOW GRANTS FOR user_0d_1@localhost;
--- script that lists all privileges of user "user_0d_2"
 SHOW GRANTS FOR user_0d_2@localhost;
