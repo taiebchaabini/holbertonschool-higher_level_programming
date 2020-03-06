20c1e84a320b5ef1452f545f22b377a920bcda8e
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 22:25:46 +0000

    First commit - new file

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,3 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+SELECT t.title, SUM(tsr.rate) as rating FROM tv_shows t LEFT JOIN tv_show_ratings tsr ON t.id = tsr.show_id GROUP BY t.title ORDER BY rating DESC;
