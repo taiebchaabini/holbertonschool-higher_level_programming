e51b4f18abc6bae3761f8d467cc76b478970dcc7
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 09:09:25 +0000

    Changing description

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
--- SELECT * from states 
+-- SELECT c.id and c.name from states where the cities state id is equal to states id
 SELECT c.id, c.name FROM cities c LEFT JOIN states s ON c.state_id = s.id WHERE c.state_id = 1;
