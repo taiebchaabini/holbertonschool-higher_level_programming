c37ccd7a128d821f6f74dae3aab3c79321c1fdde
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 08:27:25 +0000

    Changing user privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATABASE hbtn_0d_2 IF NO EXISTS;
--- CREATE USER user_0d_2
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- GRANT SELECT PRIVILEGES
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- CREATE USER user_0d_2 / GRANT PRIVILEGES SELECT ON USER
 GRANT SELECT PRIVILEGES ON *.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
