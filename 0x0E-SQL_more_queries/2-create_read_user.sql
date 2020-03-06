559c07a9286de677707a38934c76f67f66ed9ccd
taiebchaabini <taiebchaabini@gmail.com>
2020-03-06 08:30:51 +0000

    Correction to privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- CREATE USER user_0d_2 / GRANT PRIVILEGES SELECT ON USER
-GRANT SELECT PRIVILEGES ON *.* TO user_0d_2@localhost;
+GRANT SELECT ON *.* TO user_0d_2@localhost;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
