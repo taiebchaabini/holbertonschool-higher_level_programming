6e753d0b4bba0a19f8f44b2ab6810d083d0c1bf1
taiebchaabini <taiebchaabini@gmail.com>
2020-03-28 12:55:53 +0000

    Using format for execution

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     pwd = argv[2]
     dbname = argv[3]
     keyword = str(argv[4])
-    query = """SELECT * FROM states WHERE name = %s ORDER BY id"""
+    query = "SELECT * FROM states WHERE name ='{}' ORDER BY id"
     db = MySQLdb.connect(host="localhost", user=user, passwd=pwd, db=dbname)
     cur = db.cursor()
-    cur.execute(query, (keyword,))
+    cur.execute(query.format(keyword))
     rows = cur.fetchall()
     for row in rows:
         print(row)
