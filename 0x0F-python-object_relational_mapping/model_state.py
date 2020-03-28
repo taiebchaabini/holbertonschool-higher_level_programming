fec859423584ecc96da59dbd9b2be6d6543bbc4e
taiebchaabini <taiebchaabini@gmail.com>
2020-03-28 17:07:47 +0000

    Removing import

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,6 @@
 Base = declarative_base():"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 class State(Base):
