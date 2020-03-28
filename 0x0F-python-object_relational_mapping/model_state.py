98f360754acd2148d257fac1da5c47e1f779f185
taiebchaabini <taiebchaabini@gmail.com>
2020-03-28 17:07:28 +0000

    Removing connector + engine creation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,18 +4,9 @@ Base = declarative_base():"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sys import argv
 
-user = argv[1]
-pwd = argv[2]
-dbname = argv[3]
 Base = declarative_base()
-
-
 class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    connector = "mysql+mysqldb://{}:{}@localhost/{}".format(user, pwd, dbname)
-    engine = create_engine(connector)
-    Base.metadata.create_all(engine)
