b07f23d06e92c351005e5e134a61801f6206de44
taiebchaabini <taiebchaabini@gmail.com>
2020-03-28 14:04:20 +0000

    Changing to inner join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=user, passwd=pwd, db=dbname)
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
-            FROM states LEFT JOIN cities ON\
+            FROM states INNER JOIN cities ON\
             states.id = cities.state_id ORDER BY cities.id")
     rows = cur.fetchall()
     for row in rows:
