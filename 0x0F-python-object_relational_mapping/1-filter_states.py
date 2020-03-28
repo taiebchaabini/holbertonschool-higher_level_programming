1a77f7886a43fc3d86ed3871052c606a4b17e3b5
taiebchaabini <taiebchaabini@gmail.com>
2020-03-28 11:55:27 +0000

    Handling first letter only

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=user, passwd=pwd, db=dbname)
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states WHERE SUBSTRING(name, 1)\
-            REGEXP BINARY '[N]' ORDER BY id")
+            REGEXP BINARY '^[N]' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
