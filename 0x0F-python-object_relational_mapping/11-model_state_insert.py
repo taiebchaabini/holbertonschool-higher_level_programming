822f5d1069ee4195183d649835ecc06b68562f3f
taiebchaabini <taiebchaabini@gmail.com>
2020-03-28 20:34:35 +0000

    Changing desc of script

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that prints the first State object from the database hbtn_0e_6_usa
+""" script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 if __name__ == "__main__":
     from sys import argv
