0263d688302fd54a2c596f5dc926a344e5bc80b9
taiebchaabini <taiebchaabini@gmail.com>
2020-03-29 19:17:56 +0000

    New fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -18,4 +18,5 @@ if __name__ == "__main__":
     state = State(name="California")
     state.cities.append(City(name="San Fransico"))
     session.add(state)
+    query = session.query(State).first()
     session.commit()
