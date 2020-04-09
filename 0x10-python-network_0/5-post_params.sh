5d267d57db08541affb2a1b558714b7575e5a016
taiebchaabini <taiebchaabini@gmail.com>
2020-04-09 13:24:29 +0000

    Changing values

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -X POST "$1" -d "email='hr@holbertonschool.com'&subject='I will always be here for PLD'"
+curl -s -X POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
