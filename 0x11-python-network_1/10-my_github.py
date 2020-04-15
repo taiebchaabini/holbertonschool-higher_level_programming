67e5aaf13ba3c1e6e85a1d1caa437703d6240344
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 16:09:55 +0000

    Changing desc

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,9 +2,8 @@
 import requests
 import sys
 """
-Python script that takes in a URL and an email address, sends a POST request to
-the passed URL with the email as a parameter, and finally displays the body of
-the response.
+Python script that takes your Github credentials (username and password) and
+uses the Github API to display your id
 """
 if __name__ == "__main__":
     url = "https://api.github.com/user"
