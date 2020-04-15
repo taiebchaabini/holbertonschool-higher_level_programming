6c7e6f88c2113755bb3fb6734ead645faeeaf741
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 12:09:51 +0000

    Undo

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,10 +3,9 @@ import requests
 """
 Python script that fetches https://intranet.hbtn.io/status
 """
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print('Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}'.format(type(text), text))
+url = "https://intranet.hbtn.io/status"
+r = requests.get(url)
+text = r.text
+print('Body response:\n\
+\t- type: {}\n\
+\t- content: {}'.format(type(text), text))
