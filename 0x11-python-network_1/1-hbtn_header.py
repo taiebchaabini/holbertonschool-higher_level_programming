14313af2f33612fc5fc59f2a11a3f3ef3f6361e5
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 12:05:20 +0000

    Adding condition

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,8 @@ import sys
 Python script that takes in a URL, sends a request to the URL and displays the
 value of the X-Request-Id variable found in the header of the response.
 """
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('X-Request-Id'))
