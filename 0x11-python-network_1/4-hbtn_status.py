511c3cb9d7e4e8ade0f4ed36eaea07409317f856
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 12:08:59 +0000

    Adding condition

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,9 +3,10 @@ import requests
 """
 Python script that fetches https://intranet.hbtn.io/status
 """
-url = "https://intranet.hbtn.io/status"
-r = requests.get(url)
-text = r.text
-print('Body response:\n\
-\t- type: {}\n\
-\t- content: {}'.format(type(text), text))
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
+    print('Body response:\n\
+    \t- type: {}\n\
+    \t- content: {}'.format(type(text), text))
