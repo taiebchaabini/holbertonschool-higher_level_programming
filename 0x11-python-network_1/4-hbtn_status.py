a51f7c244a51c4e60c6989a3e0164f86757a8239
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 11:58:05 +0000

    New file

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+url = "https://intranet.hbtn.io/status"
+r = requests.get(url)
+text = r.text
+print('Body response:\n\
+\t- type: {}\n\
+\t- content: {}'.format(type(text), text))
