93f15ceedcb9318e671708a0e00f83d5dc8d1b2a
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 15:42:07 +0000

    New file

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+import sys
+"""
+Python script that takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter, and finally displays the body of
+the response.
+"""
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    token = sys.argv[2]
+    logins = (username, token)
+    r = requests.get(url, auth=logins)
+    print(r.json()['id'])
