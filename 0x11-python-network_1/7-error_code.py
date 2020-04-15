bb3820ea031c84afc9a1dc3996791b74dcd9e2b9
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 13:32:34 +0000

    Changing method

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,11 @@ the passed URL with the email as a parameter, and finally displays the body of
 the response.
 """
 if __name__ == "__main__":
-    url = sys.argv[1]
-    r = requests.get(url)
     try:
+        url = sys.argv[1]
+        r = requests.get(url)
+        status_code = r.status_code
+        raisecheck = r.raise_for_status()
         print(r.text)
     except requests.exceptions.HTTPError:
-        print('Error code:', r.status_code)
+        print('Error code:', status_code)
