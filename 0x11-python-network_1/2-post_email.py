b315ec320d97fecdf82b6166411aa5359e4f5bb8
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 11:14:15 +0000

    New file

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+"""
+Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+url = sys.argv[1]
+parameter = {'email': sys.argv[2]}
+data = urllib.parse.urlencode(parameter)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    print(response.read().decode('ascii'))
