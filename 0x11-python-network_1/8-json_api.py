4c3767f2f41e919723e5490da099af22935e010b
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 13:58:41 +0000

    New file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests
+import sys
+"""
+Python script that takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter, and finally displays the body of
+the response.
+"""
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        query = sys.argv[1]
+    except IndexError:
+        query = ""
+    q = {'q': query}
+    r = requests.post(url, data=q)
+    try:
+        r.json()
+    except:
+        r = None
+    try:
+        res = r.json()
+        print("[{}] {}".format(res['id'], res['name']))
+    except:
+        print("No result")
