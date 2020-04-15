3ad7a3186bb539c75b186ab64be3f54c38c85971
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 12:37:23 +0000

    New file

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+import sys
+"""
+Python script that takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter, and finally displays the body of
+the response.
+"""
+if __name__ == "__main__":
+    url = sys.argv[1]
+    mail = sys.argv[2]
+    data = {'mail': mail}
+    r = requests.post(url, data = data)
+    print(r.text)
