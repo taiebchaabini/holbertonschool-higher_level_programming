e70a3310c851e2a1e3e7afb5bd0ba1961ad44bbb
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 12:07:04 +0000

    Adding condition

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,10 +5,11 @@ import sys
 Python script that takes in a URL, sends a request to the URL and displays the
 body of the response (decoded in utf-8).
 """
-url = sys.argv[1]
-req = urllib.request.Request(url)
-try:
-        with urllib.request.urlopen(req) as response:
-                    print(response.read().decode('utf-8'))
-except urllib.error.URLError as e:
-        print('Error code:', e.getcode())
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+            with urllib.request.urlopen(req) as response:
+                        print(response.read().decode('utf-8'))
+    except urllib.error.URLError as e:
+            print('Error code:', e.getcode())
