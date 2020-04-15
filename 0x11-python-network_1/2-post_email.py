dfe8d5c0936db52ee14f03ff994c85b17ab9a21e
taiebchaabini <taiebchaabini@gmail.com>
2020-04-15 12:06:43 +0000

    Adding condition

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,10 +6,11 @@ Python script that takes in a URL and an email, sends a POST request to the
 passed URL with the email as a parameter, and displays the body of the
 response (decoded in utf-8)
 """
-url = sys.argv[1]
-parameter = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(parameter)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('ascii'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    parameter = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(parameter)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('ascii'))
