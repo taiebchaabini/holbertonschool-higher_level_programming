ee35c53e7e66fee3aa464b42bfa906249ed3a06c
taiebchaabini <taiebchaabini@gmail.com>
2020-05-05 13:38:51 +0000

    Correction to semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,6 @@ let i = 0;
 for (i = 0; i < process.argv[2]; i++) {
   console.log('C is fun');
 }
-if (process.argv[2] === 0 || process.argv[2] === undefined)
-{
-        console.log('Missing number of occurrences')
+if (process.argv[2] === 0 || process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
 }
