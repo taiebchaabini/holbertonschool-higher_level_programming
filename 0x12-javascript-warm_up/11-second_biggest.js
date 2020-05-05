80c20e9faa802518952cad88c3fd335103cb819c
taiebchaabini <taiebchaabini@gmail.com>
2020-05-05 15:02:34 +0000

    Correction to sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const args = [];
 process.argv.forEach((val, index) => {
-  if (index >= 2) { args.push(`${val}`); }
+  if (index >= 2) { args.push(parseInt(`${val}`)); }
 });
-if (args.length <= 1) { console.log(0); } else { console.log(args.sort().reverse()[1]); }
+if (args.length <= 1) { console.log(0); } else {
+  args.sort(function (a, b) {
+    return a - b;
+  });
+  args.reverse();
+  console.log(args[1]);
+}
