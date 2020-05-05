54e794e39ea97e86a8393a1ac13f09e11de7ae12
taiebchaabini <taiebchaabini@gmail.com>
2020-05-05 13:18:48 +0000

    New file

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let checkArgs = false;
+process.argv.forEach((val, index) => {
+  if (index >= 2) {
+    console.log(`${val}`);
+    checkArgs = true;
+  }
+});
+if (checkArgs === false) {
+  console.log('No argument');
+}
