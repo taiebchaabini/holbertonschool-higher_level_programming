8911a4e9f6b1154eee20b9b2aa05a4fe8148b46b
taiebchaabini <taiebchaabini@gmail.com>
2020-05-11 23:09:20 +0000

    New file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+var fs = require('fs');
+const args = process.argv;
+
+const file1 = fs.readFileSync(args[2], 'utf8');
+const file2 = fs.readFileSync(args[3], 'utf8');
+const data = file1 + file2;
+fs.writeFile(args[4], data, err => {
+  if (err) {
+    console.error(err);
+  }
+});
