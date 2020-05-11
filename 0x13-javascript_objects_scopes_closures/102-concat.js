eeaca8dbdc73b18eec7a0b57aec64f0292371f18
taiebchaabini <taiebchaabini@gmail.com>
2020-05-11 23:17:32 +0000

    removing var

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-var fs = require('fs');
+const fs = require('fs');
 const args = process.argv;
-
 const file1 = fs.readFileSync(args[2], 'utf8');
 const file2 = fs.readFileSync(args[3], 'utf8');
 const data = file1 + file2;
