66bfcb9b60fb5bb0379ca1dbe20770395849ff00
taiebchaabini <taiebchaabini@gmail.com>
2020-05-12 17:55:23 +0000

    Adding condition for argv

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const argv = process.argv;
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (error, request) {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log(JSON.parse(request.body).title);
-});
+if (argv.length === 3) {
+  request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (error, request) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(JSON.parse(request.body).title);
+  });
+}
