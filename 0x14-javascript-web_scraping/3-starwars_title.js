20f2732c0e51296a1577ace0b6b9d626ac0279ac
taiebchaabini <taiebchaabini@gmail.com>
2020-05-12 17:52:07 +0000

    New file

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv;
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (error, request) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(JSON.parse(request.body).title);
+});
