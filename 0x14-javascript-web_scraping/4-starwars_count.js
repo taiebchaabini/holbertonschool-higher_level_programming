f01838873de2e06f5f0d6ed0442489263a53c183
taiebchaabini <taiebchaabini@gmail.com>
2020-05-12 20:43:43 +0000

    New file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/', function (error, request) {
+  if (error) {
+    console.log();
+    return;
+  }
+  if (request.statusCode === 200) {
+    const response = JSON.parse(request.body);
+    let c = 0;
+    let characters = '';
+    for (const i in response.results) {
+      characters = response.results[i].characters;
+      for (let i = 0; i < characters.length; i++) {
+        if (characters[i] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          c += 1;
+        }
+      }
+    }
+    console.log(c);
+  }
+});
