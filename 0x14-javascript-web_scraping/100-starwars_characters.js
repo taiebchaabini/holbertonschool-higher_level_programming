baae4374db16cd532bef290a6a7512f0ef0e8c12
taiebchaabini <taiebchaabini@gmail.com>
2020-05-12 21:49:28 +0000

    Correction to output

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,27 @@
 #!/usr/bin/node
 const argv = process.argv;
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/people', function (error, request) {
+let character;
+request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (error, request) {
   if (error) {
     console.log(error);
     return;
   }
   if (request.statusCode === 200) {
     const response = JSON.parse(request.body);
-    let film;
-    let character;
-    for (let i = 0; i < response.results.length; i++) {
-      character = response.results[i];
-      for (let b = 0; b < character.films.length; b++) {
-        film = character.films[b];
-        if (film === 'https://swapi-api.hbtn.io/api/films/' + argv[2] + '/') { console.log(character.name); }
-      }
+    for (let i = 0; i < response.characters.length; i++) {
+      character = response.characters[i];
+      const request = require('request');
+      request(character, function (error, request) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        if (request.statusCode === 200) {
+          const response = JSON.parse(request.body);
+          console.log(response.name);
+        }
+      });
     }
   }
 });
